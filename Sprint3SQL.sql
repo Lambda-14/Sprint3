7105d82b27148ad72b50d9a4b939df3980253ea0
Lambda <130541423+Lambda-14@users.noreply.github.com>
2025-01-21 10:30:22 +0100

    Add files via upload

--- Sprint3SQL.sql
+++ Sprint3SQL.sql
@@ -50,12 +50,14 @@ WHERE id = "02C6201E-D90A-1859-B4EE-88D2986D3B02";
 -- Exercici 2
 -- La secció de màrqueting desitja tenir accés a informació específica per a realitzar anàlisi i estratègies efectives. S'ha sol·licitat crear una vista que proporcioni detalls clau sobre les companyies i les seves transaccions. Serà necessària que creïs una vista anomenada VistaMarketing que contingui la següent informació: Nom de la companyia. Telèfon de contacte. País de residència. Mitjana de compra realitzat per cada companyia. Presenta la vista creada, ordenant les dades de major a menor mitjana de compra.
 CREATE VIEW `VistaMarketing` AS 
-SELECT company_name, phone, country, avg(amount) AS Average
+SELECT company_name, phone, country, round(avg(amount), 2) AS Average
 FROM company
 JOIN transaction
 ON (company.id = company_id)
 GROUP BY company_name, phone, country
 ORDER BY Average desc;
+
+SELECT * FROM vistamarketing;
 
 -- Exercici 3
 -- Filtra la vista VistaMarketing per a mostrar només les companyies que tenen el seu país de residència en "Germany"
